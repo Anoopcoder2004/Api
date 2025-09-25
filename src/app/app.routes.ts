6186788509c386590bc3422a5354b4fccb6258f9
Anoop <Anooppopziyan@gmail.com>
2025-09-25 17:24:40 +0530

    added dashboard page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { WeatherApiComponent } from './weather-api/weather-api.component';
 import { CatComponent } from './cat/cat.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'weather', component: WeatherApiComponent },
   { path: 'cat',component: CatComponent},
-  { path: 'pokemon',component: PokemonComponent}
+  { path: 'pokemon',component: PokemonComponent},
+  { path: 'dashboard', component: DashboardComponent }
 
 ];
