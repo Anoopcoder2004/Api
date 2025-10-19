37635cf78f4585ddf2bd5b8471130b5fb8c4bdad
Anoop <Anooppopziyan@gmail.com>
2025-10-19 23:00:41 +0530

    project details api integration(unecessary commonmodules added)

--- src/app/project-details/project-details.component.ts
+++ src/app/project-details/project-details.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectService,Project } from '../services/project.service';
+import { AppComponent } from '../app.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-details',
   standalone: true,
-  imports: [],
+  imports: [AppComponent,CommonModule],
   templateUrl: './project-details.component.html',
   styleUrl: './project-details.component.scss'
 })
-export class ProjectDetailsComponent {
+export class ProjectDetailsComponent implements OnInit {
+projects:Project[]=[];
+
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit() {
+    this.projectService.getProjects().subscribe({
+      next: (data) => this.projects = data,
+      error: (err) => console.error('Error fetching projects', err)
+    });
+  }
 
 }
---
+++ src/app/services/project.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Project {
+  projectId: string;
+  projectCode: string;
+  projectName: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  status: string;
+  priority: string;
+  clientName: string;
+  budget: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+  private apiUrl = 'http://localhost:8080/api/projects';
+
+  constructor(private http: HttpClient) {}
+
+  getProjects(): Observable<Project[]> {
+    return this.http.get<Project[]>(this.apiUrl);
+  }
+}
